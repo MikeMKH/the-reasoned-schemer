6ade5088276e5ff69546caf55ed5a5b141296536
Mike Harris <MikeMKH@users.noreply.github.com>
2025-01-03 13:17:00 -0600

    fix prefix-in

--- ch1.rkt
+++ ch1.rkt
@@ -4,12 +4,12 @@
 (define s (== #t #t))
 (define u (== #t #f))
 
-(require (prefix-in : rackunit))
+(require (except-in rackunit fail))
 (require rackunit/text-ui)
 
 (run-tests
- (:test-suite "chapter 1"
-  (:test-equal? "7" (run* (q) u) '())
-  (:test-equal? "10" (run* (q) (== 'pea 'pod)) '())
-  (:test-equal? "11" (run* (q) (== q 'pea)) '(pea))
+ (test-suite "chapter 1"
+  (test-equal? "7" (run* (q) u) '())
+  (test-equal? "10" (run* (q) (== 'pea 'pod)) '())
+  (test-equal? "11" (run* (q) (== q 'pea)) '(pea))
  ))
