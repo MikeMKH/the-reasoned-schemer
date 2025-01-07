8ec260b3c0ff4dc6e35c5b2b500ca773b3fcde00
Mike Harris <MikeMKH@users.noreply.github.com>
2025-01-07 05:43:54 -0600

    add conj

--- ch1.rkt
+++ ch1.rkt
@@ -13,6 +13,7 @@
   (test-equal? "10" (run* (q) (== 'pea 'pod)) '())
   (test-equal? "11" (run* (q) (== q 'pea)) '(pea))
   (test-equal? "12" (run* (q) (== 'pea q)) '(pea))
+  
   (test-equal? "16" (run* (q) s) '(_.0)) ; q is still fresh
   (test-equal? "19" (run* (q) (== 'pea 'pod)) '()) ; q is still fresh
   (test-equal? "20" (run* (q) (== q q)) '(_.0)) ; q is still fresh
@@ -20,10 +21,18 @@
   (test-equal? "24" (run* (q) (fresh (x) (== 'pea x))) '(_.0)) ; q is still fresh
   (test-equal? "25" (run* (q) (fresh (x) (== (cons x '()) q))) '((_.0))) ; x is still fresh
   (test-equal? "32" (run* (q) (== '(((pea)) pod) '(((pea)) pod))) '(_.0)) ; q is still fresh
+  
   (test-equal? "33" (run* (q) (== '(((pea)) pod) '(((pea)) ,q))) '())
   (test-equal? "34" (run* (q) (== '(((,q)) pod) '(((pea)) pod))) '())
   (test-equal? "35" (run* (q) (== '(((,q)) pod) '(((,x)) pod))) '())
   (test-equal? "36" (run* (q) (== '(((,q)) ,x) '(((,x)) pod))) '())
   (test-equal? "37" (run* (q) (fresh (x) (== '(,x ,x) 'q))) '())
   (test-equal? "38" (run* (q) (fresh (x) (fresh (y) (== '(,q ,y) '((,x ,y) ,x))))) '())
+
+  (test-equal? "50" (run* (q) (conj s s)) '(_.0))
+  (test-equal? "51" (run* (q) (conj s (== 'corn q))) '(corn))
+  (test-equal? "52" (run* (q) (conj u (== 'corn q))) '())
+  (test-equal? "53" (run* (q) (conj (== 'corn q) (== 'meal q))) '())
+  (test-equal? "54" (run* (q) (conj (== 'corn q) (== 'corn q))) '(corn))
+  
  ))
