c969010eca229d8051b417ed98c5d1c98b45fb63
Mike Harris <MikeMKH@users.noreply.github.com>
2025-01-13 06:05:04 -0600

    add 12-25

--- ch2.rkt
+++ ch2.rkt
@@ -20,6 +20,16 @@
   (fresh (d)
     (== (cons a d) p)))
 
+; 13
+(defrel (cdro p d)
+  (fresh (a)
+    (== (cons a d) p)))
+
+; 25
+(defrel (conso a d p)
+  (caro p a)
+  (cdro p d))
+
 (run-tests
  (test-suite "chapter 2"
   (test-equal? "1" (car '(grape rasin pear)) 'grape)
@@ -31,7 +41,18 @@
   (test-equal? "5" (run* (r) (fresh (x y) (caro (cons r y) x) (== 'pear x))) '(pear))
   (test-equal? "7" (cons (car '(grape rasin pear)) (car '((a) (b) (c)))) '(grape a))
   (test-equal? "8" (run* (r) (fresh (x y) (caro '(grape rasin pear) x) (caro '((a) (b) (c)) y) (== (cons x y) r))) '((grape a)))
-
   (test-equal? "10" (cdr '(grape rasin pear)) '(rasin pear))
   (test-equal? "11" (car (cdr (cdr '(a c o r n)))) 'o)
+  
+  ; cdro is defrel outside of run-tests
+  (test-equal? "12" (run* (r) (fresh (v) (cdro '(a c o r n) v) (fresh (w) (cdro v w) (caro w r)))) '(o))
+  (test-equal? "15" (run* (r) (fresh (x y) (cdro '(grape rasin pear) x) (caro '((a) (b) (c)) y) (== (cons x y) r))) '(((rasin pear) a)))
+  (test-equal? "16" (run* (q) (cdro '(a c o r n) '(c o r n))) '(_.0))
+  (test-equal? "17" (run* (x) (cdro '(c o r n) (cons x '(r n)))) '(o))
+  (test-equal? "18" (run* (l) (fresh (x) (cdro l '(c o r n)) (caro l x) (== 'a x))) '((a c o r n)))
+
+  ; conso is defrel outside of run-tests
+  (test-equal? "19" (run* (l) (conso '(a b c) '(d e) l)) '(((a b c) d e)))
+  (test-equal? "20" (run* (x) (conso x '(a b c) '(d a b c))) '(d))
+  (test-equal? "22" (run* (x) (conso x (cons 'a (cons x '(c))) (append '(d a) (cons x '(c))))) '(d))
  ))
