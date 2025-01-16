fb006263b4e419dccc0abe54bef058b100d6ab09
Mike Harris <MikeMKH@users.noreply.github.com>
2025-01-16 06:05:19 -0600

    add 1-18

--- ch3.rkt
+++ ch3.rkt
@@ -16,7 +16,49 @@
 (require (except-in rackunit fail))
 (require rackunit/text-ui)
 
+(defrel (caro p a)
+  (fresh (d)
+    (== (cons a d) p)))
+
+(defrel (cdro p d)
+  (fresh (a)
+    (== (cons a d) p)))
+
+(defrel (conso a d p)
+  (caro p a)
+  (cdro p d))
+
+(defrel (nullo x)
+  (== x '()))
+
+(defrel (pairo p)
+  (fresh (a d)
+    (conso a d p)))
+
+; 1
+(define (list? l)
+  (cond
+    ((null? l) #t)
+    ((pair? l) (list? (cdr l)))
+    (#t #f)))
+
+; 8
+(defrel (listo l)
+  (conde
+     ((nullo l) s)
+     ((fresh (d)
+      (cdro l d)
+      (listo d)))))
+
 (run-tests
  (test-suite "chapter 3"
-  (test-equal? "init" (car '(grape rasin pear)) 'grape)
+  (test-equal? "2" (list? '()) #t)
+  (test-equal? "3" (list? 's) #f)
+  (test-equal? "4" (list? '(date . s)) #f)
+
+  ; listo is defrel outside of run-tests
+  (test-equal? "9" (run* (x) (listo (append '(a b) (cons x '(d))))) '(_.0))
+  ;(test-equal? "12" (run* (x) (listo (append '(a b) x))) '(lots-of-things)) ; will cause an endless loop
+  (test-equal? "14" (run 1 (x) (listo (append '(a b) x))) '(()))
+  (test-equal? "18" (run 5 (x) (listo (append '(a b) x))) '(() (_.0) (_.0 _.1) (_.0 _.1 _.2) (_.0 _.1 _.2 _.3)))
  ))
