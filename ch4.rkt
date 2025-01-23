3e880fb2fbb9e48b6109660bf705c2ff68a374df
Mike Harris <MikeMKH@users.noreply.github.com>
2025-01-23 05:43:04 -0600

    init

---
+++ ch4.rkt
@@ -0,0 +1,22 @@
+#lang racket
+(require cKanren/miniKanren)
+
+; https://github.com/rymaju/mykanren/blob/c56318625319b598c0e05cf993e8aafabee83187/mykanren.rkt#L360C1-L366C34
+(define-syntax defrel
+  (syntax-rules ()
+    ((defrel (name x ...) g ...)
+     (define (name x ...)
+       (lambda (s)
+         (lambda ()
+           ((conj g ...) s)))))))
+
+(define s (== #t #t))
+(define u (== #t #f))
+
+(require (except-in rackunit fail))
+(require rackunit/text-ui)
+
+(run-tests
+ (test-suite "chapter 4"
+  (test-equal? "hello testing" (list? '()) #t)
+ ))
