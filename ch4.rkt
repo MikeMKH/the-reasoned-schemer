f5bedbc721615a12c3979e2242f0dead52cb63ee
Mike Harris <MikeMKH@users.noreply.github.com>
2025-01-24 06:16:03 -0600

    add 1-21

--- ch4.rkt
+++ ch4.rkt
@@ -16,7 +16,39 @@
 (require (except-in rackunit fail))
 (require rackunit/text-ui)
 
+; 1
+(define (append l t)
+  (cond
+    ((null? l) t)
+    (#t (cons (car l)
+              (append (cdr l) t)))))
+
+; 13
+(defrel (appendo l t out)
+  (conde
+   ((nullo l) (== t out))
+   ((fresh (a d res)
+      (conso a d l)
+      (appendo d t res)
+      (conso a res out)))))
+
 (run-tests
  (test-suite "chapter 4"
-  (test-equal? "hello testing" (list? '()) #t)
+  (test-equal? "1" (append '(a b c) '(d e)) '(a b c d e))
+  (test-equal? "2" (append '() '(d e)) '(d e))
+  ;(test-equal? "3" (append 'a '(d e)) '(d e)) ; car: contract violation
+  (test-equal? "2" (append '(d e) 'a) '(d e . a))
+
+  ; appendo is defrel outside of run-tests
+  (test-equal? "17" (run 6 (x) (fresh (y z) (appendo x y z))) '(() (_.0) (_.0 _.1) (_.0 _.1 _.2) (_.0 _.1 _.2 _.3) (_.0 _.1 _.2 _.3 _.4)))
+  (test-equal? "18" (run 6 (y) (fresh (x z) (appendo x y z))) '(_.0 _.0 _.0 _.0 _.0 _.0))
+  (test-equal? "20" (run 6 (z) (fresh (x y) (appendo x y z))) '(_.0 (_.0 . _.1) (_.0 _.1 . _.2) (_.0 _.1 _.2 . _.3) (_.0 _.1 _.2 _.3 . _.4) (_.0 _.1 _.2 _.3 _.4 . _.5)))
+  (test-equal? "21" (run 6 (x y z) (appendo x y z))
+    '((() _.0 _.0)
+     ((_.0) _.1 (_.0 . _.1))
+     ((_.0 _.1) _.2 (_.0 _.1 . _.2))
+     ((_.0 _.1 _.2) _.3 (_.0 _.1 _.2 . _.3))
+     ((_.0 _.1 _.2 _.3) _.4 (_.0 _.1 _.2 _.3 . _.4))
+     ((_.0 _.1 _.2 _.3 _.4) _.5 (_.0 _.1 _.2 _.3 _.4 . _.5))))
+  ; 22
  ))
