244f80869d4263a02e26386d9812204d1e01c346
Mike Harris <MikeMKH@users.noreply.github.com>
2025-01-31 06:00:48 -0600

    add 1-13

--- ch6.rkt
+++ ch6.rkt
@@ -16,7 +16,25 @@
 (require (except-in rackunit fail))
 (require rackunit/text-ui)
 
+; 1
+(defrel (alwayso)
+  (conde
+   (s)
+   ((alwayso))))
+
 (run-tests
  (test-suite "chapter 6"
-  (test-equal? "init" (cons 'fig '(roll)) '(fig roll))
+  ; alwayso is defrel outside of run-tests
+  (test-equal? "1" (run 1 (q) (alwayso)) '(_.0))
+  (test-equal? "2" (run 1 (q) (conde (s) ((alwayso)))) '(_.0))
+  ;(test-equal? "3" (run* (q) (alwayso)) '(_.0 'endlessly))
+  ;(test-equal? "4" (run* (q) (conde (s) ((alwayso)))) '(_.0 'endlessly))
+  (test-equal? "6" (run 5 (q) (alwayso)) '(_.0 _.0 _.0 _.0 _.0))
+  (test-equal? "7" (run 5 (q) (== 'onion q) (alwayso)) '(onion onion onion onion onion))
+  ;(test-equal? "8" (run 1 (q) (alwayso) u) '('endlessly-nothing))
+  (test-equal? "9" (run 1 (q) (== 'garlic q) s (== 'onion q)) '())
+  ;(test-equal? "10" (run 1 (q) (== 'garlic q) (alwayso) (== 'onion q)) '('endlessly-nothing))
+  (test-equal? "11" (run 1 (q) (conde ((== 'garlic q) (alwayso)) ((== 'onion q))) (== 'onion q)) '(onion))
+  ;(test-equal? "12" (run 2 (q) (conde ((== 'garlic q) (alwayso)) ((== 'onion q))) (== 'onion q)) '(onion endlessly-nothing))
+  (test-equal? "13" (run 5 (q) (conde ((== 'garlic q) (alwayso)) ((== 'onion q) (alwayso))) (== 'onion q)) '(onion onion onion onion onion))
  ))
