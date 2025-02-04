78ede6352605b7a2efe59dc1d0f624d7194260b6
Mike Harris <MikeMKH@users.noreply.github.com>
2025-02-04 05:53:27 -0600

    add 1-13

--- ch7.rkt
+++ ch7.rkt
@@ -16,7 +16,38 @@
 (require (except-in rackunit fail))
 (require rackunit/text-ui)
 
+; 5
+(defrel (bit-xoro x y r)
+  (conde
+   ((== 0 x) (== 0 y) (== 0 r))
+   ((== 0 x) (== 1 y) (== 1 r))
+   ((== 1 x) (== 0 y) (== 1 r))
+   ((== 1 x) (== 1 y) (== 0 r))))
+
+; 10
+(defrel (bit-ando x y r)
+  (conde
+   ((== 0 x) (== 0 y) (== 0 r))
+   ((== 0 x) (== 1 y) (== 0 r))
+   ((== 1 x) (== 0 y) (== 0 r))
+   ((== 1 x) (== 1 y) (== 1 r))))
+
+; 12
+(defrel (half-addero x y r c)
+  (bit-xoro x y r)
+  (bit-ando x y c))
+
 (run-tests
  (test-suite "chapter 7"
-  (test-equal? "init" (run 1 (q) s) '(_.0))
+  ; bit-xoro is defrel outside of run-tests
+  (test-equal? "6" (run* (x y) (bit-xoro x y 0)) '((0 0) (1 1)))
+  (test-equal? "8" (run* (x y) (bit-xoro x y 1)) '((0 1) (1 0)))
+  (test-equal? "9" (run* (x y r) (bit-xoro x y r)) '((0 0 0) (0 1 1) (1 0 1) (1 1 0)))
+
+  ; bit-ando is defrel outside of run-tests
+  (test-equal? "11" (run* (x y) (bit-ando x y 1)) '((1 1)))
+
+  ; half-addero is defrel outside of run-tests
+  (test-equal? "12" (run* (r) (half-addero 1 1 r 1)) '(0))
+  (test-equal? "13" (run* (x y r c) (half-addero x y r c)) '((0 0 0 0) (0 1 1 0) (1 0 1 0) (1 1 0 1)))
  ))
