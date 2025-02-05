e3bef7d593ae639c13667209e22cd93e9379fac6
Mike Harris <MikeMKH@users.noreply.github.com>
2025-02-05 06:09:16 -0600

    add 15-43

--- ch7.rkt
+++ ch7.rkt
@@ -37,6 +37,22 @@
   (bit-xoro x y r)
   (bit-ando x y c))
 
+; 15
+(defrel (full-addero b x y r c)
+  (fresh (w xy wz)
+    (half-addero x y w xy)
+    (half-addero w b r wz)
+    (bit-xoro xy wz c)))
+
+; 43
+(define (build-num n)
+  (cond
+    ((odd? n)
+     (cons 1 (build-num (/ (- n 1) 2))))
+    ((and (not (zero? n)) (even? n))
+     (cons 0 (build-num (/ n 2))))
+    ((zero? n) '())))
+
 (run-tests
  (test-suite "chapter 7"
   ; bit-xoro is defrel outside of run-tests
@@ -50,4 +66,28 @@
   ; half-addero is defrel outside of run-tests
   (test-equal? "12" (run* (r) (half-addero 1 1 r 1)) '(0))
   (test-equal? "13" (run* (x y r c) (half-addero x y r c)) '((0 0 0 0) (0 1 1 0) (1 0 1 0) (1 1 0 1)))
+
+  ; full-addero is defrel outside of run-tests
+  (test-equal? "15" (run* (r c) (full-addero 0 1 1 r c)) '((0 1)))
+  (test-equal? "16" (run* (r c) (full-addero 1 1 1 r c)) '((1 1)))
+  (test-equal? "17" (run* (b x y r c) (full-addero b x y r c))
+    '((0 0 0 0 0)
+      (1 0 0 1 0)
+      (0 0 1 1 0)
+      (1 0 1 0 1)
+      (0 1 0 1 0)
+      (0 1 1 0 1)
+      (1 1 0 0 1)
+      (1 1 1 1 1)))
+
+  ; build-num is define outside of run-tests
+  (test-equal? "21" (build-num 0) '())
+  (test-equal? "23" (build-num 1) '(1))
+  (test-equal? "24" (build-num 5) '(1 0 1))
+  (test-equal? "25" (build-num 7) '(1 1 1))
+  (test-equal? "26" (build-num 9) '(1 0 0 1))
+  (test-equal? "28" (build-num 6) '(0 1 1))
+  (test-equal? "30" (build-num 19) '(1 1 0 0 1))
+  (test-equal? "31" (build-num 1729) '(1 0 0 0 0 0 1 1 0 1 1))
+  (test-equal? "40" (build-num 36) '(0 0 1 0 0 1))
  ))
