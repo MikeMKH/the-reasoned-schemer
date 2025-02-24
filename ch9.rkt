ee7c7691ff14b27785f8490be729730be301c514
Mike Harris <MikeMKH@users.noreply.github.com>
2025-02-24 05:51:30 -0600

    add 1-12

--- ch9.rkt
+++ ch9.rkt
@@ -16,7 +16,24 @@
 (require (except-in rackunit fail))
 (require rackunit/text-ui)
 
+; 11
+(defrel (not-pastao x)
+  (conda
+   ((== 'pasta x) u)
+   (s s)))
+
 (run-tests
  (test-suite "chapter 9"
-  (test-equal? "init" (run* (q) (== (+ 1 2 3) 6)) '(_.0))
+  (test-equal? "1" (run* (q) (conda (u s) (s u))) '())
+  (test-equal? "2" (run* (q) (conda (u s) (s s))) '(_.0))
+  (test-equal? "3" (run* (q) (conda (s u) (s s))) '())
+  (test-equal? "4" (run* (q) (conda (s s) (s u))) '(_.0))
+  (test-equal? "5" (run* (x) (conda ((== 'olive x) s) (s (== 'oil x)))) '(olive))
+  (test-equal? "7" (run* (x) (conda ((== 'virgin x) u) ((== 'olive x) s) (s (== 'oil x)))) '())
+  (test-equal? "8" (run* (q) (fresh (x y) (== 'split x) (== 'pea y)  (conda ((== 'split x) (== x y)) (s s)))) '())
+  (test-equal? "9" (run* (q) (fresh (x y) (== 'split x) (== 'pea y)  (conda ((== x y) (== 'split x)) (s s)))) '(_.0))
+
+  ; not-pastao is defrel outside of run-tests
+  (test-equal? "11" (run* (x) (conda ((not-pastao x) u) ((== 'spaghetti x) s))) '(spaghetti))
+  (test-equal? "12" (run* (x) (== 'spaghetti x) (conda ((not-pastao x) u) ((== 'spaghetti x) s))) '())
  ))
