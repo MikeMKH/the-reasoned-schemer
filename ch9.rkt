47d0280e037473e170eeeac66188701a0bc81bdb
Mike Harris <MikeMKH@users.noreply.github.com>
2025-02-26 06:01:09 -0600

    add 21-40

--- ch9.rkt
+++ ch9.rkt
@@ -13,6 +13,58 @@
 (define s (== #t #t))
 (define u (== #t #f))
 
+(defrel (bit-xoro x y r)
+  (conde
+   ((== 0 x) (== 0 y) (== 0 r))
+   ((== 0 x) (== 1 y) (== 1 r))
+   ((== 1 x) (== 0 y) (== 1 r))
+   ((== 1 x) (== 1 y) (== 0 r))))
+
+(defrel (bit-ando x y r)
+  (conde
+   ((== 0 x) (== 0 y) (== 0 r))
+   ((== 0 x) (== 1 y) (== 0 r))
+   ((== 1 x) (== 0 y) (== 0 r))
+   ((== 1 x) (== 1 y) (== 1 r))))
+
+(defrel (half-addero x y r c)
+  (bit-xoro x y r)
+  (bit-ando x y c))
+
+(defrel (full-addero b x y r c)
+  (fresh (w xy wz)
+    (half-addero x y w xy)
+    (half-addero w b r wz)
+    (bit-xoro xy wz c)))
+
+(defrel (addero b n m r)
+  (conde
+   ((== 0 b) (== '() m) (== n r))
+   ((== 0 b) (== '() n) (== m r) (poso m))
+   ((== 1 b) (== '() m) (addero 0 n '(1) r))
+   ((== 1 b) (== '() n) (poso m) (addero 0 '(1) m r))
+   ((== '(1) n) (== '(1) m)
+     (fresh (a c)
+       (== `(,a ,c) r)
+       (full-addero b 1 1 a c)))
+   ((== '(1) n) (gen-addero b n m r))
+   ((== '(1) m) (>1o n) (>1o r) (addero b '(1) n r))
+   ((>1o n) (gen-addero b n m r))))
+
+(defrel (gen-addero b n m r)
+  (fresh (a c d e x y z)
+    (== `(,a . ,x) n)
+    (== `(,d . ,y) m) (poso y)
+    (== `(,c . ,z) r) (poso z)
+    (full-addero b a d c e)
+    (addero e x y z)))
+
+(defrel (+o n m k)
+  (addero 0 n m k))
+
+(defrel (-o n m k)
+  (+o k m n))
+
 (require (except-in rackunit fail))
 (require rackunit/text-ui)
 
@@ -21,6 +73,35 @@
   (conda
    ((== 'pasta x) u)
    (s s)))
+
+; 20
+(defrel (teacupo t)
+  (conde
+   ((== 'tea t))
+   ((== 'cup t))))
+
+; 21
+(defrel (onceo g)
+  (condu
+   (g s)
+   (s u)))
+
+; 26
+(defrel (bumpo n x)
+  (conde
+   ((== n x))
+   ((fresh (m)
+      (-o n '(1) m)
+      (bumpo m x)))))
+
+; 27
+(defrel (gen&test+o i j k)
+  (onceo
+   (fresh (x y z)
+     (+o x y z)
+     (== i x)
+     (== j y)
+     (== k z))))
 
 (run-tests
  (test-suite "chapter 9"
@@ -37,10 +118,22 @@
   (test-equal? "11" (run* (x) (conda ((not-pastao x) u) ((== 'spaghetti x) s))) '(spaghetti))
   (test-equal? "12" (run* (x) (== 'spaghetti x) (conda ((not-pastao x) u) ((== 'spaghetti x) s))) '())
   ;(test-equal? "13" (run* (q) (conda (alwayso s) (s u))) '(endless-loop)) ; builds an infinite list of _.0
-
   (test-equal? "14" (run* (q) (condu (alwayso s) (s u))) '(_.0))
   ;(test-equal? "15" (run* (q) (condu (s alwayso) (s u))) '(endless-loop))
   ;(test-equal? "17" (run 1 (q) (conda (alwayso s) (s u)) u) '(endless-loop)) ; continously fails
-  (test-equal? "17" (run 1 (q) (condu (alwayso s) (s u)) u) '())
-  
+  (test-equal? "18" (run 1 (q) (condu (alwayso s) (s u)) u) '())
+
+  ; teacupo and onceo are defrel outside of run-tests
+  (test-equal? "21" (run* (x) (onceo(teacupo x))) '(tea))
+  (test-equal? "22" (run* (r) (conde ((teacupo r) s) ((== #f r) s))) '(tea cup)) ; text has '(#f tea cup)
+  (test-equal? "23" (run* (r) (conda ((teacupo r) s) (s (== #f r)))) '(tea cup))
+  (test-equal? "24" (run* (r) (== #f r) (conda ((teacupo r) s) ((== #f r) s) (s u))) '()) ; text has '(#f)
+  (test-equal? "25" (run* (r) (== #f r) (condu ((teacupo r) s) ((== #f r) s) (s u))) '()) ; text has '(#f)
+
+  ; bumpo is defrel outside of run-tests
+  (test-equal? "26" (run* (x) (bumpo '(1 1 1) x)) '((1 1 1) (0 1 1) (1 0 1) (0 0 1) (1 1) (0 1) (1) ()))
+
+  ; gen&test+o is defrel outside of run-tests
+  (test-equal? "27" (run* (q) (gen&test+o '(0 0 1) '(1 1) '(1 1 1))) '(_.0))
+  ;(test-equal? "40" (run 1 (q) (gen&test+o '(0 0 1) '(1 1) '(0 1 1))) '(endless-loop)) ; continously fails
  ))
